d13c234e1f713c69af0e1383701607e961704527
jonghechoi <sosinnmi@naver.com>
2023-05-01 14:06:14 +0900

    2023-05-01 (코딩테스트) 가장큰수 보충

--- my_project/src/coding_test/가장큰수.java
+++ my_project/src/coding_test/가장큰수.java
@@ -1,7 +1,7 @@
 package coding_test;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class 가장큰수 {
 
@@ -10,51 +10,52 @@ public class 가장큰수 {
 //		int[] numbers = {5, 9, 3, 30, 34};
 //		int[] numbers = {333, 30, 34, 5, 9};
 //		int[] numbers = {313, 30, 34, 5, 9};
-//		int[] numbers = {900, 30, 34, 5, 9};
+//		int[] numbers = {900, 30, 50, 510, 9};
 //		int[] numbers = {0, 30, 34, 5, 9};
 //		int[] numbers = {0, 10, 34, 5, 9};
 //		int[] numbers = {707, 77, 34, 5, 9};
-		int[] numbers = {777, 30, 77, 5, 9};
+//		int[] numbers = {777, 30, 77, 5, 9};
 //		int[] numbers = {6, 10, 2};
 //		int[] numbers = {0, 10, 2};
-//		int[] numbers = {0, 0};
-		ArrayList<Integer> answerArray = new ArrayList<>();
+		int[] numbers = {00, 0, 0, 0};
+//		int[] numbers = {0, 0, 12};
+//		int[] numbers = {34, 3444, 32, 3222};
+		
+		String[] strNumbers = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
+		
+		ArrayList<String> answerArray = new ArrayList<>();
 
 		boolean except = true;
-		for(int i=0; i<numbers.length; i++) { // 새로운 값
-			if(numbers[i] != 0) except = false;
+		for(int i=0; i<strNumbers.length; i++) { // 새로운 값
+			if(!strNumbers[i].equals("0")) except = false;
 			
 			if(answerArray.isEmpty()) {
-				answerArray.add(numbers[i]);
+				answerArray.add(strNumbers[i]);
 			}else {
 				int count = 0;
-				String nextStr = String.valueOf(numbers[i]);
 				for(int j=0; j<answerArray.size(); j++) { // 기존에 담겨 있던 값
-					String prevStr =  String.valueOf(answerArray.get(j));
-					
-					int prevNext = Integer.valueOf( prevStr + nextStr ); 
-					int nextPrev = Integer.valueOf( nextStr + prevStr );
-					
-					if(prevNext < nextPrev) {
+					int compareNum = (strNumbers[i]+answerArray.get(j)).compareTo(answerArray.get(j)+strNumbers[i]);
+
+					if(compareNum < 0) {
+						count = j+1;
+					}else if(compareNum >= 0) {
 						count = j;
 						j = answerArray.size(); // nextPrev가 크면 뒷자리수 확인 필요 없이 그 자리에 넣음
-					}else {
-						count = j+1;
 					}
 				}
-				answerArray.add(count, numbers[i]);
+				answerArray.add(count, strNumbers[i]);
 			}
 		}
 		
-		System.out.println(answerArray);
-		
 		String answer = ""; 
-		if(except) {
-			answer = "0";
+		System.out.println(except);
+		if(!except) {
+			for(String num : answerArray) answer += String.valueOf(num);
 		}else {
-			for(int num : answerArray ) answer += String.valueOf(num);
+			answer = "0";
 		}
-		
 		
 		System.out.println(answer);
 	}
